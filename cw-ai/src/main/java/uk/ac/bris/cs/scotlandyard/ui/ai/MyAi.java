9a4397769fafc507f65ffeb548a53be7748699dc
jammy-jar <jrose04.0821@gmail.com>
2024-03-29 10:50:01 +0000

    Implement basic test 'AI'

--- cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,8 +1,13 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.Ai;
 import uk.ac.bris.cs.scotlandyard.model.Board;
@@ -10,13 +15,21 @@ import uk.ac.bris.cs.scotlandyard.model.Move;
 
 public class MyAi implements Ai {
 
-	@Nonnull @Override public String name() { return "Name me!"; }
+	@Nonnull @Override public String name() { return "Jammar"; }
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
 		// returns a random move, replace with your own implementation
-		var moves = board.getAvailableMoves().asList();
-		return moves.get(new Random().nextInt(moves.size()));
+
+		ImmutableList<Move> moves = board.getAvailableMoves().asList();
+		int[] moveScores = moves.stream().mapToInt(m -> new MyScoreBoardFactory().build(board).getScore(m)).toArray();
+		int indexOfMax = 0;
+		for (int i = 0; i < moveScores.length; i++) {
+			if (moveScores[i] > moveScores[indexOfMax])
+				indexOfMax = i;
+		}
+
+		return moves.get(indexOfMax);
 	}
 }
